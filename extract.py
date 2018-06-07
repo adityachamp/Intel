64e8fe3b6bc33556331f5f942c9f4c2ec5d504c9
adityachamp <31718627+adityachamp@users.noreply.github.com>
2018-06-07 19:41:50 -0400

    final code

--- extract.py
+++ extract.py
@@ -22,10 +22,8 @@ def header_file_extraction(header_file_directory):
 		#for key,val in mydict.items():                			#### PRINTING ENTIRE DICTIONARY
 		#	print key, val
 
-def huawei_rwrite_extraction(huawei_file_directory): 	
-	dict2 = {}
-	pattern = re.compile(r'rwrite[(a-z0-9_,]+ [A-Za-z0-9)]+')
 
+'''
 	with open(huawei_file_directory,'r') as f:
 		contents = f.read()
 		#print contents
@@ -45,10 +43,39 @@ def huawei_rwrite_extraction(huawei_file_directory):
 		lookup_addr(dict2)
 		#print len(dict2)
 		return dict2 
-		
 		#print dict2.keys()
 
+'''
+def huawei_rwrite_extraction(huawei_file_directory): 	
+	pattern = re.compile(r'rwrite[(a-z0-9,]+ [A-Za-z0-9)]+')
+	with open(huawei_file_directory,'r') as f:
+		for line in f:
+			dict2 = {}
+			#print line	
+			matches = pattern.match(line)
+			if matches:
+				#print 'entered match'
+				#print match.group()                    ###### MATCHING THE PATERN
+				spl = (matches.group(0).split(" "))
+				addr = spl[0][7:-1]
+				data = spl[1][:-1]
+				#print data
+				str(addr)
+				#print addr
+				int_val_txt = int(addr[2:],16)							####converting hex to int 	
+				dict2[int_val_txt] = data
+				#print int_val
+				#print dict2
+				lookup_addr(dict2)
+				#print len(dict2)
+			else:
+				#print 'entered else'
+				out_file.write (line + '\n')
+				#pass
+
+
 def lookup_addr(dict2):
+	#print dict2
 	for key in dict2.keys():
 		if(True):
 			pass
@@ -56,16 +83,20 @@ def lookup_addr(dict2):
 				#print 'checking'   										
 				if addr == key:
 					#print 'entered'
-					print name , ' ' + ' ' + dict2[key]
+					out_file.write( name + ' ' + ' ' + dict2[key] + '\n')
 					#pass
 		else:
-			print 'address ' + str(key) + ' not found in header file '
+			out_file.write('address ' + str(key) + ' not found in header file\n ')
 
-#mydict = header_file_extraction('/nfs/sc/disks/ngs_proj_workspace001/singhad1/scripting_python/header/nr_top_32bit.h')		
-#dict2 = huawei_rwrite_extraction('/nfs/sc/disks/ngs_proj_workspace001/singhad1/scripting_python/programming/huawei_csirs_trs_gryffindor_rmi.txt')
 
+out_file = open('out.txt','w')
 mydict = header_file_extraction(sys.argv[1])		
 dict2 = huawei_rwrite_extraction(sys.argv[2])
 
-
+#mydict = header_file_extraction('/nfs/sc/disks/ngs_proj_workspace001/singhad1/scripting_python/header/nr_top_32bit.h')		
+#dict2 = huawei_rwrite_extraction('/nfs/sc/disks/ngs_proj_workspace001/singhad1/scripting_python/programming/huawei_csirs_trs_gryffindor_rmi.txt')
 #lookup_addr(int(sys.argv[1]),dict2[int(sys.argv[1])])
+
+
+
+
